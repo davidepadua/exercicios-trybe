c80d66cfc378fa7ab193424788a30250c6621a31
davidepadua <davidepadua1914@gmail.com>
2020-02-03 19:56:49 -0300

    Initial Commit

--- exercicio11.sh
+++ exercicio11.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#10. Escreva um Shell Script que renomeia todos os arquivos do diretório atual que terminam em ".png" 
-#de forma que o nome do arquivo comece com a data atual no formato "YYYY-MM-DD". 
-#Por exemplo, se tiver um arquivo com o nome "kitten.png" no diretório atual, e a data de hoje for 16/04/2020,
-#o script deverá mudar o nome do arquivo para "2020-04-16-kitten.png".
+#11
 
 DIRETORIO=$1
 EXTENSION=$2
