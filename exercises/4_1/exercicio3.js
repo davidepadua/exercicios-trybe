1d6bc88691a3c6ac8d97548ae543c1f6e308f06f
davidepadua <davidepadua1914@gmail.com>
2020-02-14 18:55:44 -0300

    comparador entre 3 numeros

---
+++ exercises/4_1/exercicio3.js
@@ -0,0 +1,17 @@
+//Faça um programa que retorne o maior de três números. 
+//Defina no começo do programa três variáveis com os valores que serão comparados.
+
+let a = 40;
+let b = 80;
+let c = 50;
+
+if(a>b &&a >c){
+    console.log(a)
+} 
+    else if(a>b && a<c){
+        console.log(c)
+    }
+    else{
+    console.log(b);
+}
+
