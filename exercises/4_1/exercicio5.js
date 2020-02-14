56dea0d3e82f9026465638f0ceb2ce4bdae36950
davidepadua <davidepadua1914@gmail.com>
2020-02-14 19:13:38 -0300

    true/false para angulos de triangulo

---
+++ exercises/4_1/exercicio5.js
@@ -0,0 +1,13 @@
+//Faça um programa que define três variáveis com os valores dos três angulos internos de um triangulo.
+//Retorne true se os angulos representarem os angulos de um triangulo e false caso contrário.
+
+let anguloAB = 40;
+let anguloBC = 80;
+let anguloCA = 50;
+
+if (anguloAB + anguloBC + anguloCA == 180){
+    console.log('True');
+}    else {
+    console.log('False');
+}
+
