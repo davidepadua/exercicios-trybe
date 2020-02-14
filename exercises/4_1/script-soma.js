c46fc573c62cc4e12ffa0e47666ef7c6a6a09392
davidepadua <davidepadua1914@gmail.com>
2020-02-14 18:33:20 -0300

    script de subtracao

---
+++ exercises/4_1/script-soma.js
@@ -0,0 +1,10 @@
+//Faça cinco programas, um para cada operação arimética básica. 
+//Seu programa deve ter duas variáveis a e b definidas 
+//no começo com os valores que serão operados. Faça programas para:
+//Adição,Subtracão,Multiplicação,Divisão,Módulo
+
+let a = 5;
+let b = 10;
+
+console.log(a+b);
+
