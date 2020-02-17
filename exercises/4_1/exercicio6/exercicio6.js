4eac47f970069e8271f6181ee787222cfb174310
davidepadua <davidepadua1914@gmail.com>
2020-02-17 11:37:54 -0300

    Descricao peca Xadrez

---
+++ exercises/4_1/exercicio6/exercicio6.js
@@ -0,0 +1,31 @@
+let msg;
+    function letrasiguais(){        
+        let texto = document.getElementById('peca').value; 
+        let textoMin = texto.toLowerCase();
+        
+       console.log(textoMin); 
+       switch(textoMin){
+           case "peão":
+                 msg="O Peão são as peças de front, seu movimento varia em 2 casas (caso seja primeiro movimento)<br> ou uma casa adiante nas demais";
+                 break;
+           case "cavalo":
+                 msg="O Cavalo se movimenta em formato de L,";
+                 break
+           case "torre":
+                 msg="A Torre se movimenta Horizontalmente e Verticalmente quantas casas puder.";
+                 break;
+           case "bispo": 
+                 msg="O Bispo se movimenta em diagonais";
+                 break;    
+           case "rei":
+                 msg="O Rei se movimenta apenas em suas casas adjacentes.";
+                 break;
+           case "rainha":
+                 msg="A Rainha se movimenta para onde quiser quantas casas puder.";
+            default: 
+                 msg="Não é uma peça válida."
+       } 
+       document.getElementById("descricao").innerHTML = msg;     
+       
+       }
+       
